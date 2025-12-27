ec2f056b3dc7a340c21e6c3b2124e1be44c3c1f5
Ayush Katiyar <143071145+ayushkatiyar31@users.noreply.github.com>
2025-12-27 19:36:36 +0530

    2058-CONCATENATION-OF-ARRAY Stats: Time: 0 ms (100%), Space: 17 MB (41.85%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.cpp
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        vector<int>ans;
-      
-      for(int i = 0; i < nums.size(); i++){
-        ans.push_back(nums[i]);
-      }
-     
-     for(int i = 0 ;i < nums.size();i++){
-        ans.push_back(nums[i]);
-     }
-     return ans;
-     
+        vector<int>ans = nums;
+
+        ans.insert(ans.end(),nums.begin(),nums.end());
+        return ans;
         
     }
 };
