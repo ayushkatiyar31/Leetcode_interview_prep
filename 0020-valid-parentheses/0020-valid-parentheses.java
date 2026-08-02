e8c32444e595a596b4d4347d57373a5f180f6c5a
Ayush Katiyar <143071145+ayushkatiyar31@users.noreply.github.com>
2026-08-02 18:20:41 +0530

    0020-VALID-PARENTHESES Stats: Time: 2 ms (98.24%), Space: 43 MB (93.58%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,28 @@
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
-        
+
         Stack<Character> stack = new Stack<>();
 
-        for(int i = 0; i < s.length(); i++){
+        for (char ch : s.toCharArray()) {
 
-            char ch = s.charAt(i);
-
-            if(ch == '(' || ch =='{' || ch == '['){
-                stack.push(ch);
-            } else {
-                if(stack.isEmpty()){
+            if (ch == '(') {
+                stack.push(')');
+            }
+            else if (ch == '{') {
+                stack.push('}');
+            }
+            else if (ch == '[') {
+                stack.push(']');
+            }
+            else {
+                if (stack.isEmpty() || stack.pop() != ch) {
                     return false;
                 }
-
-                char top = stack.pop();
-
-                if((ch == ')' && top != '(') || 
-                    (ch == '}' && top != '{') ||
-                    (ch == ']' && top != '[')) {
-                        return false;
-                    }
             }
         }
+
         return stack.isEmpty();
     }
 }
